01c144ba30929c3d911bb5478efb1ec5f6a29791
Ghaniaufa <maulaghani.alaufa@gmail.com>
2026-07-12 15:01:32 +0700

    change the URL link for 'Dashboard'

--- cypress/e2e/orangehrm-dashboard.cy.js
+++ cypress/e2e/orangehrm-dashboard.cy.js
@@ -1,7 +1,7 @@
 describe('OrangeHRM Login Feature', () => {
 
     beforeEach(() => {
-        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
         cy.url().should('include', '/auth/login')
     })
 
