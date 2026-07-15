3e8613ebfe55b8b249cdecd2fd599bda926f65b4
Ghaniaufa <maulaghani.alaufa@gmail.com>
2026-07-15 12:28:35 +0700

    create TC001 - TC003 for Testing POM Script

---
+++ cypress/e2e/orangehrm-loginPOM.cy.js
@@ -0,0 +1,37 @@
+import LoginPage from '../pages/LoginPage'
+import data from '../fixtures/loginData.json'
+
+const login = new LoginPage()
+
+describe('OrangeHRM Login POM', () => {
+    //TC001
+    it('TC001 - Login Success', () => {
+
+        login.visit()
+        login.inputUsername(data.validUser)
+        login.inputPassword(data.validPassword)
+        login.clickLogin()
+        login.verifyDashboard()
+
+    })
+    //TC002
+    it('TC002 Empty Username',()=>{
+
+        login.visit()
+        login.inputUsername(data.empty)
+        login.inputPassword(data.validPassword)
+        login.clickLogin()
+        cy.contains('Required').should('be.visible')
+
+    })
+    it('TC003 Empty Password',() => {
+
+        login.visit()
+        login.inputUsername(data.validPassword)
+        login.inputPassword(data.empty)
+        login.clickLogin()
+        cy.contains('Required').should('be.visible')
+    })
+
+
+})
