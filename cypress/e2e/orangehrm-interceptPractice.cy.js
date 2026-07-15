9e48a41facbc542f1f2e9d7ee9bb963cfcf4e2e5
Ghaniaufa <maulaghani.alaufa@gmail.com>
2026-07-15 11:23:26 +0700

    Intercept practice use with intercept method for Login

---
+++ cypress/e2e/orangehrm-interceptPractice.cy.js
@@ -0,0 +1,21 @@
+describe('OrangeHRM Login', () => {
+
+    it('Login berhasil', () => {
+
+        cy.intercept('POST','**/auth/validate').as('loginRequest')
+
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
+        cy.get('input[name="username"]').type('Admin')
+        cy.get('input[name="password"]').type('admin123')
+
+        cy.get('button[type="submit"]').click()
+
+        cy.wait('@loginRequest')
+
+        cy.url().should('include','dashboard')
+
+    })
+    
+
+})
