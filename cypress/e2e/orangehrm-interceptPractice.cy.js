3c1fd82ddda07f6fc71bd2cdef777ae4fe2605b5
Ghaniaufa <maulaghani.alaufa@gmail.com>
2026-07-15 12:12:44 +0700

    create TC010 & TC011 for intercept 'spaceUsername' & 'spacePassword'

--- cypress/e2e/orangehrm-interceptPractice.cy.js
+++ cypress/e2e/orangehrm-interceptPractice.cy.js
@@ -88,5 +88,35 @@ describe('OrangeHRM Login using Intercept', () => {
 
         cy.contains('Invalid credentials').should('be.visible')
     })
+    // TC010
+    it('TC010 - Login with leading Username spacing', () => {
+         cy.intercept('POST','**/auth/validate').as('spaceUsername')
+
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
+        cy.get('input[name="username"]').type(' Admin ')
+        cy.get('input[name="password"]').type('admin123')
+        cy.get('button[type="submit"]').click()
+
+        cy.wait('@spaceUsername')
+
+        cy.contains('Invalid credentials').should('be.visible')
+    })
+    // TC011
+    it('TC011 - Login with leading Password spacing', () => {
+         cy.intercept('POST','**/auth/validate').as('spacePassword')
+
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
+        cy.get('input[name="username"]').type('Admin')
+        cy.get('input[name="password"]').type(' admin123 ')
+        cy.get('button[type="submit"]').click()
+
+        cy.wait('@spacePassword')
+
+        cy.contains('Invalid credentials').should('be.visible')
+    })
+
+    
 
 })
